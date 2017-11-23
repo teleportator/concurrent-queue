35089335a7507d4d990172c781fb85ac4952b9fd
teleportator <teleportator@ya.ru>
2017-11-23 15:43:36 +0300

    Initial commit.

---
+++ ConcurrentQueue.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections;
+
+namespace ConcurrentQueue
+{
+	public class ConcurrentQueue<T>
+	{
+		private readonly Queue _internalQueue;
+
+		public ConcurrentQueue()
+		{
+			_internalQueue = new Queue();
+		}
+
+		public void Push(T item)
+		{
+			lock (_internalQueue.SyncRoot)
+			{
+				_internalQueue.Enqueue(item);
+			}
+		}
+
+		public T Pop()
+		{
+			while (true)
+			{
+				try
+				{
+					return SyncronizedDequeue();
+				}
+				catch (InvalidOperationException)
+				{
+				}
+			}
+		}
+
+		private T SyncronizedDequeue()
+		{
+			lock (_internalQueue.SyncRoot)
+			{
+				return (T) _internalQueue.Dequeue();
+			}
+		}
+	}
+}
---
+++ ConcurrentQueueTests.cs
@@ -0,0 +1,56 @@
+﻿using System.Linq;
+using System.Threading;
+using System.Threading.Tasks;
+using FluentAssertions;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace ConcurrentQueue
+{
+	[TestClass]
+	public class ConcurrentQueueTests
+	{
+		[TestMethod, Timeout(5000)]
+		public void ConcurrentQueue_ShouldBeFifo()
+		{
+			var sut = new ConcurrentQueue<int>();
+
+			var count = 10;
+			var sequence = Enumerable.Repeat(0, count).Select((n, i) => i).ToArray();
+
+			foreach (var item in sequence)
+			{
+				sut.Push(item);
+			}
+
+			var actual = new int[10];
+			for (int i = 0; i < count; i++)
+			{
+				actual[i] = sut.Pop();
+			}
+
+			actual.ShouldAllBeEquivalentTo(sequence);
+		}
+
+		[TestMethod, Timeout(5000)]
+		public void ConcurrentQueue_Pop_ShouldWaitItemInQueue()
+		{
+			var sut = new ConcurrentQueue<int>();
+			var manualResetEvent = new ManualResetEvent(false);
+			var popTask = Task.Run(
+				() =>
+				{
+					manualResetEvent.Set();
+					return sut.Pop();
+				});
+
+			manualResetEvent.WaitOne();
+			popTask.Wait(1000).Should().BeFalse("expected to wait for item in queue");
+
+			sut.Push(1);
+			popTask.Wait(1000).Should().BeTrue("expected item to be dequeue immediately");
+
+			var actual = popTask.Result;
+			actual.Should().Be(1);
+		}
+	}
+}
